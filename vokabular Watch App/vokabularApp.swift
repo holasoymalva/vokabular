4daccad28b3fd9c94a0be7a33f51c1fe8a84a245
holasoymalva <alquimista3301@gmail.com>
2025-01-13 23:11:30 -0600

    Initial Commit

---
+++ vokabular Watch App/vokabularApp.swift
@@ -0,0 +1,17 @@
+//
+//  vokabularApp.swift
+//  vokabular Watch App
+//
+//  Created by malva on 13/01/25.
+//
+
+import SwiftUI
+
+@main
+struct vokabular_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ vokabular Watch AppTests/vokabular_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  vokabular_Watch_AppTests.swift
+//  vokabular Watch AppTests
+//
+//  Created by malva on 13/01/25.
+//
+
+import Testing
+@testable import vokabular_Watch_App
+
+struct vokabular_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
